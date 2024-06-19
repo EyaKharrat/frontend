54a693e75c766266e84572afd6e579444d7b0b9d
eya Kharrat <eyakharrat2017@gmail.com>
2024-06-19 15:56:41 +0100

    dashbord

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from "react";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
+import "./App.scss";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from "./components/Navbar.js";
 import Registre from "./components/Registre";
 import Login from "./components/Login";
 import Home from "./components/Home";
+import Sidebar from "./components/sidebar/Sidebar.jsx";
+import Logout from "./components/Logout.js";
 
 function App() {
+  
   return (
     <div>
       <Router>
-        <Navbar />
+     
         <Routes>
+          <Route path='/dhashbord' element={<Sidebar />} />
           <Route path='/registre' element={<Registre />} />
           <Route path='/login' element={<Login />} />
           <Route path='/home' element={<Home />} />
+          <Route path='/logout' element={<Logout />} />
         </Routes>
+       
       </Router>
     </div>
   );
